1c46f95d582d6b95267d6fe2c706e6ae686466b5
sergiojc90 <sergiojc90@gmail.com>
2021-05-30 23:43:08 -0500

    Creating and using new module myName.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from "lodash";
+import myName from "./myName";
 
 function component(){
     const element = document.createElement("div");
+    const element2 = document.createElement("div");
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(["Hello","webpack"], " ");
+    
+    // myName, now imported by this script
+    element2.innerHTML = myName("Sergio");
 
     return element;
 }
